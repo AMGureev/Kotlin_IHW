6d3bbf3ca22cd29d4c74e46131b26ad3020f0f57
Гуреев Александр Михайлович <130486442+AMGureev@users.noreply.github.com>
2023-12-24 12:35:39 +0300

    Update AccountService.kt
    add commits

--- src/main/kotlin/ru/hse/cinema/AccountService.kt
+++ src/main/kotlin/ru/hse/cinema/AccountService.kt
@@ -5,7 +5,7 @@ import ru.hse.cinema.dao.AccountDao
 class AccountService(
     private val accountDao: AccountDao
 ) {
-    fun registerUser(name: String, password: String) {
+    fun registerUser(name: String, password: String) { // process register user
         if (accountDao.findAccountByLogin(name)) {
             println("An account with this username already exists!")
         } else {
@@ -14,7 +14,7 @@ class AccountService(
         }
     }
 
-    fun authenticateUser(name: String, inputPassword: String): Boolean {
+    fun authenticateUser(name: String, inputPassword: String): Boolean { // authenticate user : return true or false
         return if (accountDao.findAccountByLogin(name)) {
             if (accountDao.authenticateUser(inputPassword, accountDao.returnAccountByName(name))) {
                 println("Access granted!")
@@ -29,11 +29,11 @@ class AccountService(
         }
     }
 
-    fun saveAllInformationAboutAccountsToJson() {
+    fun saveAllInformationAboutAccountsToJson() { // save all information about accounts to Json file
         accountDao.saveAllAccounts()
     }
 
-    fun initialFillingOfAccountsFile() {
+    fun initialFillingOfAccountsFile() { // get information from json file
         accountDao.fillingAccountsData()
     }
 }
